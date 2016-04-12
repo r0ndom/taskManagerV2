91a1230f2bc2df496e2248460c3f201bf6554d5f
r0ndom <r0ndom@yahoo.com>
2016-04-12 09:11:52 +0300

    added openshift  db

--- src/main/java/ua/pb/task/manager/config/DataSourceConfig.java
+++ src/main/java/ua/pb/task/manager/config/DataSourceConfig.java
@@ -16,7 +16,10 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
 * Created by Mednikov on 26.02.2016.
@@ -27,18 +30,38 @@ import java.io.IOException;
 @EnableTransactionManagement
 public class DataSourceConfig {
 
+    public static final String REMOTE_HOST = System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
+    public static final String REMOTE_USERNAME = System.getenv("OPENSHIFT_POSTGRESQL_DB_USERNAME");
+    public static final String REMOTE_PASSWORD = System.getenv("OPENSHIFT_POSTGRESQL_DB_PASSWORD");
+    public static final String REMOTE_PORT = System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
+
     @Autowired
     private Environment env;
     @Autowired
     private ApplicationContext context;
 
+    private Properties connectionProperties;
+
+    public DataSourceConfig() throws IOException {
+        connectionProperties = new Properties();
+
+        //TODO need to find more readable way
+        connectionProperties.load(new FileInputStream(getClass().getResource("/db.properties").getFile()));
+
+        if (REMOTE_HOST != null) {
+            connectionProperties.put("db.url", getRemoteUrl());
+            connectionProperties.put("db.username", REMOTE_USERNAME);
+            connectionProperties.put("db.password", REMOTE_PASSWORD);
+        }
+    }
+
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(env.getProperty("db.driver"));
-        dataSource.setUrl(env.getProperty("db.url"));
-        dataSource.setUsername(env.getProperty("db.username"));
-        dataSource.setPassword(env.getProperty("db.password"));
+        dataSource.setUrl(connectionProperties.getProperty("db.url"));
+        dataSource.setUsername(connectionProperties.getProperty("db.username"));
+        dataSource.setPassword(connectionProperties.getProperty("db.password"));
         return dataSource;
     }
 
@@ -56,6 +79,10 @@ public class DataSourceConfig {
         DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
         transactionManager.setDataSource(dataSource());
         return transactionManager;
+    }
+
+    private String getRemoteUrl() {
+        return "db:postgresql://" + REMOTE_HOST + ":" + REMOTE_PORT;
     }
 
 }
